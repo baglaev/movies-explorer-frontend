87690fffa29d0ba797b2892db05f9b7bce257f3f
baglaev <bglvssh@yandex.ru>
2023-11-10 03:44:25 +0300

    link fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.bglvssh.diploma.nomoredomainsrocks.ru/';
+export const BASE_URL = 'https://api.bglvssh.diploma.nomoredomainsrocks.ru';
 // export const BASE_URL = 'http://localhost:3000';
 
 const getResponseData = (res) => {
