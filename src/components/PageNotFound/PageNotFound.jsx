62dec50920524f7a3cd534861e455f2a7fc401b2
baglaev <bglvssh@yandex.ru>
2023-11-11 12:51:12 +0300

    404 and span error

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function PageNotFound() {
+
+  function backPage() {
+    window.history.go(-1);
+  }
   
   return (
     <main className="not-found">
       <section className="not-found__container">
         <p className="not-found__number">404</p>
         <p className="not-found__title">Страница не найдена</p>
-        <Link className="not-found__link" to="/">Назад</Link>
+        <button  className="not-found__link" type="button" onClick={backPage}>Назад</button >
       </section>
     </main>
   )
